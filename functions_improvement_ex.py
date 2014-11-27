6088adc8be506ee90b1b91aa651ae2aa93d2ff7a
kerslaket <44108@longroad.ac.uk>
2014-11-27 21:28:42 +0000

    :)

--- functions_improvement_ex.py
+++ functions_improvement_ex.py
@@ -1,5 +1,6 @@
 # functions improvement exercise
 # times-table tester
+
 import random
 
 def timesTable():
@@ -9,26 +10,35 @@ def timesTable():
     testTable = int(testTable)
     return testTable
 
+def questionComplete(testTable):
+    for questions in range(1,6):
+        question(testTable)
+        displayCorrect(UserAnswer,Ans)
+        
 def question(testTable):
-    
-
-
-    
-# main program
-print("Times-table tester")
-print()
-testTable = input("Which times-table do you want to be tested on? ")
-testTable = int(testTable)
-for questions in range(1,6):
     Num1 = testTable
     Num2 = random.randrange(2,13)
     Ans = Num1 * Num2
     UserAnswer = input(str(Num1) + ' x ' + str(Num2) + ' = ? ')
     UserAnswer = int(UserAnswer)
-    if UserAnswer == Ans:
+    return UserAnswer, Ans
+    
+def displayCorrect(UserAnswer,Ans):
+     if UserAnswer == Ans:
         print('Well done, you got the correct answer!')
         print()
-    else:
+     else:
         print('Sorry, you got the answer wrong. The correct answer is',Ans)
         print()
               
+def timesTableTester():
+    tableTest = timesTable()
+    questionComplete(tableTest)
+
+#main program
+timesTableTester()
+    
+
+
+    
+
